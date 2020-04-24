b71f1419e0743331fa1032471b5b408b6e88fa83
Nur Hazirah Abdullah <hazirah.abdullah462@gmail.com>
2020-04-24 17:43:15 +0800

    Update recipeDB.js

--- recipeDB.js
+++ recipeDB.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'password5197_',
+    password: 'passwordxxxx',
     database: 'recipe_app'
 });
 
